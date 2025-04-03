dd8a497da4bf7f980ae7ff2b09f8fd63ff011959
janaka99 <janaka.chathuranga@ideahub.lk>
2025-04-03 20:46:30 +0530

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace wedding_planer_ad.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
